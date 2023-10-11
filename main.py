87f6c8cc43cabab9a3ed3894d7b3116c8b3ab9cb
Ece Ozalp <eceiozalp@gmail.com>
2023-10-11 10:29:23 -0700

    Removed train

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ import os
 app = FastAPI()
 
 
-@app.get('/train')
-async def train():
-  data_processing = create_data_processing()
+# @app.get('/train')
+# async def train():
+#   data_processing = create_data_processing()
   
-  with open('dp.joblib', 'wb') as f:
-    joblib.dump(data_processing,f)
+#   with open('dp.joblib', 'wb') as f:
+#     joblib.dump(data_processing,f)
 
-  # Initialize an S3 client
-  s3 = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
+#   # Initialize an S3 client
+#   s3 = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
 
-  # Upload the file to S3
-  s3.upload_file('dp.joblib', 'demotlbucket', 'dp.joblib')
+#   # Upload the file to S3
+#   s3.upload_file('dp.joblib', 'demotlbucket', 'dp.joblib')
 
-  model = Model()
-  model.split()
-  model.fit()
+#   model = Model()
+#   model.split()
+#   model.fit()
 
-  joblib.dump(model, "model.joblib")
-  s3.upload_file('model.joblib', 'demotlbucket', 'model.joblib')
-  return {"success"}
+#   joblib.dump(model, "model.joblib")
+#   s3.upload_file('model.joblib', 'demotlbucket', 'model.joblib')
+#   return {"success"}
 
 
 class PredictRequest(BaseModel):
